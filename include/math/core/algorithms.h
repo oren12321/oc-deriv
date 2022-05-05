27e5a6d5c33626693f51ccb9a8272bd3baea7c93
oren12321 <oren12321@gmail.com>
2022-05-05 13:11:48 +0300

    Add initial idea for reverse automatic differentiation

--- include/math/core/algorithms.h
+++ include/math/core/algorithms.h
@@ -18,4 +18,84 @@ namespace math::algorithms {
     }
 }
 
+namespace math::algorithms::derivatives {
+
+    class Const {
+    public:
+        Const(double value)
+            : value_(value) {}
+
+        double compute()
+        {
+            return value_;
+        }
+
+        double backward(std::size_t)
+        {
+            return 0.0;
+        }
+    private:
+        double value_;
+    };
+
+    class Var {
+    public:
+        Var(double value, std::size_t index)
+            : value_(value), index_(index) {}
+
+        double compute()
+        {
+            return value_;
+        }
+
+        double backward(std::size_t index)
+        {
+            return index_ == index ? 1.0 : 0.0;
+        }
+    private:
+        double value_;
+        std::size_t index_;
+    };
+
+    template <typename T, typename U>
+    class Add {
+    public:
+        Add(T x, U y)
+            : x_(x), y_(y) {}
+
+        double compute()
+        {
+            return x_.compute() + y_.compute();
+        }
+
+        double backward(std::size_t index)
+        {
+            return x_.backward(index) + y_.backward(index);
+        }
+    private:
+        T x_;
+        U y_;
+    };
+
+    template <typename T, typename U>
+    class Mul {
+    public:
+        Mul(T x, U y)
+            : x_(x), y_(y) {}
+
+        double compute()
+        {
+            return x_.compute() * y_.compute();
+        }
+
+        double backward(std::size_t index)
+        {
+            return x_.backward(index) * y_.compute() + x_.compute() * y_.backward(index);
+        }
+    private:
+        T x_;
+        U y_;
+    };
+}
+
 #endif // MATH_ALGORITHMS_H
--- test/algorithms.cpp
+++ test/algorithms.cpp
@@ -16,3 +16,20 @@ TEST(Algorithms_test, two_numbers_can_be_compared_with_specified_percision)
     EXPECT_TRUE(is_equal(c, d));
     EXPECT_FALSE(is_equal(c, d, 1.0e-15));
 }
+
+TEST(test, test)
+{
+    using namespace math::algorithms::derivatives;
+
+    Var x(3.0, 0);
+    Var y(2.0, 1);
+    auto z = Add(
+        Add(
+            Mul(x, x),
+            Mul(Const(3.0), Mul(x, y))),
+        Const(1.0));
+
+    double v = z.compute();
+    double d = z.backward(1);
+}
+
